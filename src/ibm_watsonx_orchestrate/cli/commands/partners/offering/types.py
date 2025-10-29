f52952d116b57ba012b878045445f524eaa084ff
Darragh McGonigle <darragh.mcgonigle@ibm.com>
2025-10-29 16:50:59 +0000

    feat(partners): Add placeholder warnings for new fields

--- src/ibm_watsonx_orchestrate/cli/commands/partners/offering/types.py
+++ src/ibm_watsonx_orchestrate/cli/commands/partners/offering/types.py
@@ -44,8 +44,48 @@ class OfferingRelatedLink(BaseModel):
     value: Optional[str]
     type: Optional[str]
 
+
+    def __eq__(self, other):
+            if isinstance(other, dict):
+                return self.model_dump() == other
+            return super().__eq__(other)
+
+
+
+class OfferingFormFactor(BaseModel):
+    aws: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
+    ibm_cloud: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
+    cp4d: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
+
+class OfferingPartNumber(BaseModel):
+    aws: Optional[str] = CATALOG_PLACEHOLDERS['part_number']
+    ibm_cloud: Optional[str] = CATALOG_PLACEHOLDERS['part_number']
+    cp4d: Optional[str] = None
+
+class OfferingScope(BaseModel):
+    form_factor: Optional[OfferingFormFactor] = OfferingFormFactor()
+    tenant_type: Optional[dict] = CATALOG_PLACEHOLDERS['tenant_type']
+
+class OfferingAgentScope(BaseModel):
+    form_factor: Optional[OfferingFormFactor] = OfferingFormFactor()
+
+class OfferingAgentBilling(BaseModel):
+    metered: bool = False
+
+class OfferingAgentRole(str, Enum):
+    MANAGER = 'manager'
+    COLLABORATOR = 'collaborator'
+
+    def __str__(self):
+        return self.value 
+
+    def __repr__(self):
+        return repr(self.value)
+    
 AGENT_CATALOG_ONLY_PLACEHOLDERS = {
     'icon': "inline-svg-of-icon",
+    'part_number': OfferingPartNumber(),
+    'scope': OfferingAgentScope(),
     'related_links': [
         OfferingRelatedLink(
             key="support",
@@ -85,36 +125,6 @@ class AgentKind(str, Enum):
     def __repr__(self):
         return repr(self.value)
 
-class OfferingFormFactor(BaseModel):
-    aws: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
-    ibm_cloud: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
-    cp4d: Optional[str] = CATALOG_PLACEHOLDERS['form_factor']
-
-class OfferingPartNumber(BaseModel):
-    aws: Optional[str] = CATALOG_PLACEHOLDERS['part_number']
-    ibm_cloud: Optional[str] = CATALOG_PLACEHOLDERS['part_number']
-    cp4d: Optional[str] = None
-
-class OfferingScope(BaseModel):
-    form_factor: Optional[OfferingFormFactor] = OfferingFormFactor()
-    tenant_type: Optional[dict] = CATALOG_PLACEHOLDERS['tenant_type']
-
-class OfferingAgentScope(BaseModel):
-    form_factor: Optional[OfferingFormFactor] = OfferingFormFactor()
-
-class OfferingAgentBilling(BaseModel):
-    metered: bool = False
-
-class OfferingAgentRole(str, Enum):
-    MANAGER = 'manager'
-    COLLABORATOR = 'collaborator'
-
-    def __str__(self):
-        return self.value 
-
-    def __repr__(self):
-        return repr(self.value)
-
 class OfferingAgentExtras(BaseModel):
     tags: Optional[List[str]] = None
     publisher: Optional[str] = None
@@ -147,9 +157,9 @@ class OfferingAgentExtras(BaseModel):
         if "agent_role" not in agent_data:
             extras.agent_role = OfferingAgentRole.MANAGER.value if agent_data.get("name") == parent_agent_name else OfferingAgentRole.COLLABORATOR.value
         if "part_number" not in agent_data:
-            extras.part_number = OfferingPartNumber()
+            extras.part_number = AGENT_CATALOG_ONLY_PLACEHOLDERS["part_number"]
         if "scope" not in agent_data:
-            extras.scope = OfferingAgentScope()
+            extras.scope = AGENT_CATALOG_ONLY_PLACEHOLDERS["scope"]
         if "channels" not in agent_data:
             extras.channels = []
         if "related_links" not in agent_data:
