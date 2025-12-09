c45dbfe6191f44fb689ebbf1c267f885e0d7e836
Benen Bruen <benenbruen@ibm.com>
2025-12-09 10:27:14 +0000

    fix/tool schema parsing fix (#2390)
    * move run param extraction into spec generation
    
    * add tests to validate context

---
+++ tests/cli/resources/python_samples/tool_w_single_context_param.py
@@ -0,0 +1,10 @@
+from ibm_watsonx_orchestrate.agent_builder.tools import tool
+from ibm_watsonx_orchestrate.run.context import AgentRun
+
+
+@tool(
+  description="Mock tool for testing context passing with request context.",
+)
+def context_mock(current_run : AgentRun) -> str:
+
+  return f"Current Context: {current_run}"
