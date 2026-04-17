9839c0316d737d60305cd0308c5768c70a8a33dc
aju-jacob <aju.jacob@ibm.com>
2026-04-17 16:27:52 +0530

    Moving files over to new folder

--- packages/agentic-sdk/ibm_watsonx_orchestrate_sdk/__init__.py
+++ packages/agentic-sdk/ibm_watsonx_orchestrate_sdk/__init__.py
@@ -4,9 +4,23 @@ __all__ = [
     "ContextClient",
     "ExecutionContext",
     "MemoryClient",
+    "Tracer",
+    "TracerConfig",
+    "trace_call",
+    "trace_llm_call",
+    "trace_tool_call",
+    "trace_agent_call",
 ]
 
 from ibm_watsonx_orchestrate_sdk.client import AgenticSDK, Client
 from ibm_watsonx_orchestrate_sdk.common.session import ExecutionContext
 from ibm_watsonx_orchestrate_sdk.context.context_client import ContextClient
 from ibm_watsonx_orchestrate_sdk.memory.memory_client import MemoryClient
+from ibm_watsonx_orchestrate_sdk.observability.config import TracerConfig
+from ibm_watsonx_orchestrate_sdk.observability.decorators import (
+    trace_agent_call,
+    trace_call,
+    trace_llm_call,
+    trace_tool_call,
+)
+from ibm_watsonx_orchestrate_sdk.observability.tracer import Tracer
