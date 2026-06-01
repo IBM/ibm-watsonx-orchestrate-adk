8821c1de77a61ebea34cb7d7deee144f19026148
Vikram Melkote <Vikram.Melkote@ibm.com>
2026-06-01 12:48:29 -0700

    fix URL based on value in dev-conn

--- packages/agentic-sdk/ibm_watsonx_orchestrate_sdk/langchain/chat_models.py
+++ packages/agentic-sdk/ibm_watsonx_orchestrate_sdk/langchain/chat_models.py
@@ -226,15 +226,13 @@ class ChatWxO(ChatOpenAI):
         # Session base_url format by mode:
         # - local: {instance_url}/api/v1 -> need to add /orchestrate
         # - runs-elsewhere: {instance_url}/v1/orchestrate
-        # - runs-on: {api_proxy_url}/instances/orchestrate
+        # - runs-on: {api_proxy_url}/instances/orchestrate/v1
         api_base_url = f"{agentic_session.base_url}"
         if local or agentic_session.mode == "local":
             api_base_url += "/orchestrate"
-        elif api_base_url.startswith("https://wo-api"):
-            # proxy base url doesn't have 'v1' in it
-            api_base_url += "/v1"
+        # add model endpoint path to URL
         api_base_url += "/gateway/model"
-        # v1/gateway/model
+
         # Configure HTTP client with SSL verification settings
         # For runs-on mode, verify is typically False to allow internal cluster communication
         http_client = None
