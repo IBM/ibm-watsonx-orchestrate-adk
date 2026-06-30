4d9ae36e061ccf9ca1fdd55e300b739160b701ef
Darragh McGonigle <Darragh.McGonigle@ibm.com>
2026-06-30 19:41:13 +0100

    feat(agents): Deprecate Agent Styles Phase 1 (#3235)
    * feat(agents): Remove deprecated agent styles from tests and update default style to be react-intrinsic
    
    * chore(examples): Update all examples to use react-intrinsic instead of react or default
    
    * chore(examples): Remove planner example
    
    * feat(agents): Add deprecation warning for agent styles
    
    * chore(agents): Add style migration guide link
    
    * feat(styles): Add new react core name alais
    
    * feat(styles): Add missing react core references
    
    * feat(styles): Add missing react core references
    
    * fix(examples): Update comments
    
    * fix(styles): Fix name on export
    
    * Update Default LLM
    
    `watsonx/meta-llama/llama-3-1-70b-instruct` from to `groq/openai/gpt-oss-120b`
    
    * Updated LLM
    
    to `groq/openai/gpt-oss-120b`
    
    * Update sample-agent.yml
    
    watsonx/openai/gpt-oss-120b no groq hosted
    
    * Update sample-agents.py
    
    watsonx/openai/gpt-oss-120b not grok hosted
    
    * Update skills/wxo-builder/SKILL.md
    
    * fix(tests): Fix failing style test
    
    * fix(styles): Fix agent create command
    
    ---------
    
    Co-authored-by: Michael Baur <Michael.Baur@ibm.com>
    Co-authored-by: Andrew Vu <Andrew.Vu@ibm.com>

--- tests/cli/resources/python_samples/sample-agents.py
+++ tests/cli/resources/python_samples/sample-agents.py
@@ -62,8 +62,8 @@ agent1 = Agent(
   
         Make sure to include decimal points when the user's input includes a float.
     """,
-    llm="watsonx/ibm/granite-3-8b-instruct", #Optional, Default=watsonx/openai/gpt-oss-120b
-    style=AgentStyle.REACT,  #Optional, Valid options = [AgentStyle.DEFAULT, AgentStyle.REACT]
+    llm="watsonx/openai/gpt-oss-120b", #Optional, Default=watsonx/openai/gpt-oss-120b
+    style=AgentStyle.REACT_CORE,  #Optional, Valid options = [AgentStyle.DEFAULT, AgentStyle.REACT, AgentStyle.REACT_CORE]
     collaborators=[
         agent2,
         "algebra_agent"
