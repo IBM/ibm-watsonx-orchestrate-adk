36bbe96f57e4f6b96fd27af8a7b71a664822209f
Rohit Sanbhadti <rohit.sanbhadti@ibm.com>
2026-08-13 15:33:43 -0700

    more review comments

--- src/ibm_watsonx_orchestrate/utils/docker_utils.py
+++ src/ibm_watsonx_orchestrate/utils/docker_utils.py
@@ -1407,7 +1407,7 @@ class DockerLoginService:
                     f"Invalid WO_INSTANCE URL: '{instance_url}'. It should contain the instance (tenant) id.")
             tenant_id = path.split('/')[-1]
             return wo_api_key, f"wxouser-{tenant_id}"
-        elif auth_type in {EnvironmentAuthType.CPD.value, EnvironmentAuthType.K8S.value}:
+        elif EnvironmentAuthType.is_cpd_like(auth_type):
             wo_api_key = env_dict.get("WO_API_KEY")
             wo_password = env_dict.get("WO_PASSWORD")
             if not wo_api_key and not wo_password:
@@ -1677,7 +1677,7 @@ class DockerComposeCore:
     def trim_cpd_image_layer_cache (self, final_env_file: Path) -> int:
         env_settings = EnvSettingsService(final_env_file)
 
-        if self.__env_service.resolve_auth_type(env_settings.get_env()) not in {EnvironmentAuthType.CPD.value, EnvironmentAuthType.K8S.value}:
+        if not EnvironmentAuthType.is_cpd_like(self.__env_service.resolve_auth_type(env_settings.get_env())):
             logger.error("Encountered a non-CPD environment. This operation can only be run for CPD.")
             sys.exit(1)
 
@@ -1834,7 +1834,7 @@ class DockerComposeCore:
         provided_registry = self.__env_service.did_user_provide_registry_url(env_settings.get_env())
 
         if (
-                self.__env_service.resolve_auth_type(env_settings.get_env()) not in {EnvironmentAuthType.CPD.value, EnvironmentAuthType.K8S.value} or
+                not EnvironmentAuthType.is_cpd_like(self.__env_service.resolve_auth_type(env_settings.get_env())) or
                 self.__env_service.did_user_provide_registry_url(env_settings.get_env())
         ):
             # no need to do custom docker image pulls when auth mode is cpd or when we're in an air-gapped cpd
